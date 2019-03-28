978933ec773ac733e29aeb2b760e335f55c7402b
Natasha K <tashk85@gmail.com>
2019-03-28 15:56:47 +1100

    showing articles setup

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,16 @@
 class ArticlesController < ApplicationController
+    def show
+        @article = Article.find(params[:id])
+    end
+    
     def new
     end
 
     def create
-        render plain: params[:article].inspect
+        @article = Article.new(params.require(:article).permit(:title, :text))
+
+        @article.save
+        redirect_to @article
     end
+
 end
