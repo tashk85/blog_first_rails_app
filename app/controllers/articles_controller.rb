68f1244b4971ea040f9f7bdb258ec13de66718bd
Natasha K <tashk85@gmail.com>
2019-03-28 16:14:02 +1100

    created links

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,16 +1,28 @@
 class ArticlesController < ApplicationController
+    def index
+        @articles = Article.all 
+    end
+    
     def show
         @article = Article.find(params[:id])
     end
     
     def new
+        @article = Article.new 
+    end
+
+    def edit
+        @article = Article.find(params[:id])
     end
 
     def create
         @article = Article.new(params.require(:article).permit(:title, :text))
 
-        @article.save
-        redirect_to @article
+        if @article.save
+            redirect_to @article
+        else
+            render 'new'
+        end
     end
 
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,4 @@
 class Article < ApplicationRecord
+    validates :title, presence: true, length: { minimum: 5 }
+    # will ensure that all articles have a title that is at least 5 characters long
 end
