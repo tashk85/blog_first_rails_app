74c71b8e14a2204e1a3af32c7e461ec3f2f9258d
Natasha K <tashk85@gmail.com>
2019-03-28 16:29:06 +1100

    updating articles functionality added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,7 +16,7 @@ class ArticlesController < ApplicationController
     end
 
     def create
-        @article = Article.new(params.require(:article).permit(:title, :text))
+        @article = Article.new(article_params)
 
         if @article.save
             redirect_to @article
@@ -25,4 +25,18 @@ class ArticlesController < ApplicationController
         end
     end
 
+    def update
+        @article = Article.find(params[:id])
+
+        if @article.update(article_params)
+            redirect_to @article
+        else
+            render 'edit'
+        end
+    end
+
+    private
+        def article_params
+            params.require(:article).permit(:title, :text)
+        end
 end
